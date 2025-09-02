4676f7883c9159bf7086d6f8c21b9f9f326f168e
Tdrummont <tdrummontt@gmail.com>
2025-09-02 15:24:43 -0300

    fix: corrigir LanguageController com métodos changeLanguage e getCurrentLanguage

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+use Inertia\Inertia;
 
 class LanguageController extends Controller
 {
-    //
+    /**
+     * Alterar o idioma da aplicação
+     */
+    public function changeLanguage(Request $request, $locale)
+    {
+        // Validar se o idioma é suportado
+        if (!in_array($locale, ['pt', 'en', 'es'])) {
+            $locale = 'pt';
+        }
+
+        // Salvar o idioma na sessão
+        Session::put('locale', $locale);
+
+        // Definir o idioma da aplicação
+        App::setLocale($locale);
+
+        // Redirecionar de volta com mensagem de sucesso
+        return redirect()->back()->with('success', __('Idioma alterado com sucesso!'));
+    }
+
+    /**
+     * Obter o idioma atual
+     */
+    public function getCurrentLanguage()
+    {
+        return response()->json([
+            'current_locale' => App::getLocale(),
+            'available_locales' => ['pt', 'en', 'es']
+        ]);
+    }
 }
